0c582eb76a033528462ae4eef4f58f16ea3ca0e6
axelmurilllo <axelmurillo@u.boisestate.edu>
2024-02-12 13:27:24 -0700

    Updated DFAState.java to add Comparable

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -2,6 +2,22 @@ package fa.dfa;
 
 import fa.State;
 
-public abstract class DFAState extends State {
-    
+public abstract class DFAState extends State implements Comparable<DFAState>{
+
+    /**
+     *
+     * @param name name for state
+     */
+    public DFAState(String name) {
+        super(name);
+    }
+
+    /**
+     *
+     * @param state the object to be compared.
+     * @return int for comparison of states, -1 if less than, 0 if equal, and 1 if greater than
+     */
+    public int compareTo(DFAState state) {
+        return state.getName().compareTo(this.getName());
+    }
 }
